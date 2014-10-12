74b92dd0b3530cf617ee07e12df36d61059ab923
Aziz Yuldoshev <aziz.yuldoshev@booking.com>
2014-10-12 21:43:18 +0200

    Move UI initialization to separate module

--- share/js/source/main.js
+++ share/js/source/main.js
@@ -3,16 +3,19 @@ require('es5-shim');
 require('es5-shim/es5-sham');
 
 var _ = require('underscore');
-var React = require('react');
-var Debugger = require('./plack-debugger');
-var Debugger_component = require('./component-debugger.jsx');
 
-var CONTAINER_ID = 'plack-debugger';
+var Debugger = require('./plack-debugger');
+var ui = require('./ui');
 
 new Debugger().ready(function() {
-    var render = create_renderer(create_container(CONTAINER_ID));
+    var CONTAINER_ID = 'plack-debugger';
+    var CSS_URL = Debugger.$CONFIG.static_url + '/css/plack-debugger.css';
 
-    inject_style_sheet(Debugger.$CONFIG.static_url + '/css/plack-debugger.css');
+    var render = ui.init({
+        containerId: CONTAINER_ID,
+        cssUrl: CSS_URL
+    });
+
     render();
 
     this.resource.on('plack-debugger.ui:load-request', function(request) {
@@ -28,28 +31,6 @@ new Debugger().ready(function() {
         render({ subrequests: subrequests });
     });
 });
-
-function create_container(id) {
-    var el = document.createElement('div');
-    el.setAttribute('id', id);
-    return document.body.appendChild(el);
-}
-
-function create_renderer(container) {
-    var _props = {};
-
-    return function(props) {
-        _.extend(_props, props);
-        React.renderComponent(Debugger_component(_props), container);
-    };
-}
-
-function inject_style_sheet(href) {
-    var link = document.createElement('link');
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('href', href);
-    return document.getElementsByTagName('head')[0].appendChild(link);
-}
 
 function check_for_ajax_tracking(request) {
     return _.some(request, function(r) { return r.metadata && !!r.metadata.track_subrequests; });
---
+++ share/js/source/ui.js
@@ -0,0 +1,46 @@
+var _ = require('underscore');
+var React = require('react');
+
+var Debugger = require('./component-debugger.jsx');
+var assert = require('./assert');
+
+var doc = document;
+
+/**
+ * Initializes debugger UI
+ * @param  {Object} options
+ * @param  {String} options.containerId  ID of the UI container element
+ * @param  {String} options.cssUrl       Optional URL of CSS file to inject
+ * @return {Function}                    UI rendering function
+ */
+exports.init = function(options) {
+    assert(_.isString(options.containerId));
+
+    var _props = {};
+    var container = createContainer(options.containerId);
+
+    if (_.isString(options.cssUrl)) {
+        injectStyleSheet(options.cssUrl);
+    }
+
+    return function(props) {
+        _.extend(_props, props);
+        React.renderComponent(Debugger(_props), container);
+    };
+};
+
+function createContainer(id) {
+    var el = doc.createElement('div');
+    el.setAttribute('id', id);
+    return doc.body.appendChild(el);
+}
+
+function injectStyleSheet(href) {
+    var link = doc.createElement('link');
+    var head = doc.getElementsByTagName('head')[0]; // IE8 compatible
+
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', href);
+
+    return head.appendChild(link);
+}
